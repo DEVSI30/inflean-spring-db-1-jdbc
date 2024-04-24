7ebd98ad02be0011cd9b83d1474bd320e7fe6e61
DEVSI30 <offset654@gmail.com>
2024-04-24 21:55:48 +0900

    JDBC 로 CRUD

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -8,7 +8,8 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 class MemberRepositoryV0Test {
@@ -23,16 +24,16 @@ class MemberRepositoryV0Test {
         Member findMember = memberRepository.findById(member.getMemberId());
         log.info("findMember={}", findMember);
 
-        Assertions.assertThat(findMember).isEqualTo(member);
+        assertThat(findMember).isEqualTo(member);
 
         // update: money: 10000 -> 2000
         memberRepository.update(member.getMemberId(), 20000);
         Member updateMember = memberRepository.findById(member.getMemberId());
-        Assertions.assertThat(updateMember.getMoney()).isEqualTo(20000);
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
 
         // delete
         memberRepository.delete(member.getMemberId());
-        Assertions.assertThatThrownBy(() -> memberRepository.findById(member.getMemberId()))
+        assertThatThrownBy(() -> memberRepository.findById(member.getMemberId()))
                 .isInstanceOf(NoSuchElementException.class);
     }
 }
